6a52667dccd9257385ee5f60d07c4afcf8bbf2a4
Maxim Razantovski <nda.mr43@gmail.com>
2023-12-20 15:29:03 +0100

    fix: link dnf to rpm-ostree when installing kmods

--- config/scripts/build-kmods.sh
+++ config/scripts/build-kmods.sh
@@ -7,6 +7,12 @@ ARCH="$(rpm -E '%_arch')"
 KERNEL="$(rpm -q "${KERNEL_NAME}" --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
 RELEASE="$(rpm -E '%fedora')"
 
+# We will link dnf to rpm-ostree for correct installation
+ln -s /usr/bin/rpm-ostree /usr/bin/dnf
+
 akmods --force --kernels "${KERNEL}" --kmod acpi_ec
 modinfo /usr/lib/modules/${KERNEL}/extra/acpi_ec/acpi_ec.ko.xz >/dev/null ||
 	(find /var/cache/akmods/acpi_ec/ -name \*.log -print -exec cat {} \; && exit 1)
+
+# Remove the link to rpm-ostree
+rm -f /usr/bin/dnf
